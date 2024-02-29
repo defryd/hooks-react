c02258383baef3a845b628041d151dcc63d01c11
Freddy Sebastian Chia Vera <freddy.chia@wiedii.co>
2024-02-29 17:00:18 -0500

    primeros avances del useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,12 @@ import './index.css'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { Padre } from './07-tarea-memo/Padre'
+// import { Padre } from './07-tarea-memo/Padre'
+import { TodoApp } from './08-useReducer/TodoApp'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Padre />
+    <TodoApp />
   </React.StrictMode>,
 )
